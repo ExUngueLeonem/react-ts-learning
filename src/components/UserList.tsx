c4b38a1228ca5aaff93296ac6351f1cb8e1d2208
Leo <ujfox13@gmail.com>
2022-02-06 19:13:17 +0300

    user list

---
+++ src/components/UserList.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import {IUser} from '../types/types';
+import UserItem from './UserItem';
+
+interface IUserList {
+    users: IUser[]
+}
+
+const UserList: FC<IUserList> = ({users}) => {
+    
+    return (
+        <div>
+            {users.map( user => <UserItem key={user.id} user={user}/>)}
+        </div>
+    );
+};
+
+export default UserList;
