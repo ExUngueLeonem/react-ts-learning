a28283b572541f6f7c651372da515d215a6fd016
Leo <ujfox13@gmail.com>
2022-02-07 15:48:45 +0300

    TodosPage

---
+++ src/pages/TodoPage/TodoPage.tsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import List from '../../components/List';
+import { ITodo } from '../../types/types';
+import TodoItem from '../../components/TodoItem'
+
+const TodoPage = () => {
+
+  //state
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    fetchTodos();
+   }, [])
+
+  async function fetchTodos() {
+    try {
+      const res = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10');
+      setTodos(res.data)
+    } catch (e) {
+      alert(e)
+    }
+  }
+
+    return (
+        <div>
+      <List
+        items={todos}
+        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />} />
+        </div>
+    );
+};
+
+export default TodoPage;
+
+
